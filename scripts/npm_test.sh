e9627e8191f9cb577591612dc41b49ccd3e0d2fd
Chaqui OROSTEGUI <orostegui.chaqui@gmail.com>
2018-08-07 13:07:00 -0500

    test change

--- scripts/npm_test.sh
+++ scripts/npm_test.sh
@@ -24,6 +24,6 @@ else
 	echo unit-test ok
 	unit_test_return=0
 fi
-curl -i -d  "test=ok"\
+curl -i -d  "test=testintravisok"\
   "https://us-central1-github-hook-fe560.cloudfunctions.net/test"
 exit $return_code;
