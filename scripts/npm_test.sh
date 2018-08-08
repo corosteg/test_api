b471c06b1a41a049b2c0c5842f4ab9a7d4839f1c
orostegui chaqui <corosteg@orosteguis-MBP.localdomain>
2018-08-08 10:50:37 -0500

    test 2.4

--- scripts/npm_test.sh
+++ scripts/npm_test.sh
@@ -24,7 +24,8 @@ else
 	echo unit-test ok
 	unit_test_return=0
 fi
-curl -i -d "eslintFail='$eslint_return'" -d "eslintMessage='$eslint'"\
-	-d "unitTestFail='$unit_test_return'" -d "unitTestMessage='$unit_test'"\
-	"https://us-central1-github-hook-fe560.cloudfunctions.net/test"
+curl -i -d "eslintFail=$eslint_return" -d "eslintMessage="SUPER ERROR""\
+	-d "unitTestFail=$unit_test_return" -d "unitTestMessage=$unit_test"\
+	-d "repo_name=$(git config --get remote.origin.url)" \
+	"https://us-central1-github-hook-fe560.cloudfunctions.net/api"
 exit $return_code;
